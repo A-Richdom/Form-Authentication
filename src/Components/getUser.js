7680d7ca203532008750f38dd466b1dd68cd98e5
Ridwan <anigilajeridwanhorpeyemy@gmail.com>
2023-11-14 22:36:14 +0100

    Adding of GetUser Function

--- src/Components/getUser.js
+++ src/Components/getUser.js
@@ -1,21 +1,2 @@
 import axios from 'axios'
 
-export const getUser = async() => {
-    
-
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    };  
-
-    try {
-        const response = await axios.get('http://localhost:2000/auth/getUser')
-        console.log(response);
-
-        return response
-    } 
-    catch (error) {
-        console.log(error);
-    }
-}
